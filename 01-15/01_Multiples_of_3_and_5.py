16c6f922fa87481e8150201eef4b627cf47454ba
Ryan Drapeau <rdrapeau1@gmail.com>
2013-03-04 15:17:56 -0800

    Finished Problem 1

--- 01-15/01_Multiples_of_3_and_5.py
+++ 01-15/01_Multiples_of_3_and_5.py
@@ -9,4 +9,20 @@ The sum of these multiples is 23.
 Find the sum of all the multiples of 3 or 5 below 1000.
 '''
 
+# Returns the possible numbers that are divisible by 3 or 5 below max
+def possible_nums(max):
+    list = []
+    for n in range(max):
+        if n % 3 == 0 or n % 5 == 0:
+            list.append(n)
+    return list
 
+# Returns the sum of the list
+def get_sum(list):
+    sum = 0
+    for n in list:
+         sum += n
+    return sum
+
+list = possible_nums(1000)
+print get_sum(list)
